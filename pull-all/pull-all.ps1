4a922bfe145ab8b3753e93331f3c71ff299298c3
dboikliev <deyanpb@hotmail.com>
2017-11-07 17:18:54 +0200

    Redeuces nesting.

--- pull-all/pull-all.ps1
+++ pull-all/pull-all.ps1
@@ -4,17 +4,17 @@ Param(
 
 if (![System.IO.Directory]::Exists($Path)) {
     Write-Error "$($Path) is not a directory."
+    exit 1
 }
-else {
-    $current = Get-Location  
-    Get-ChildItem $Path -Directory | 
-    Where-Object { Test-Path "$($_.FullName)\.git" } | 
-    ForEach-Object { 
-        Write-Output "Pulling $($_.FullName)"; 
-        Set-Location $_.FullName; 
-        git pull; 
-        Set-Location .. 
-    }; 
 
-    Set-Location $current
-}
+$current = Get-Location  
+Get-ChildItem $Path -Directory | 
+Where-Object { Test-Path "$($_.FullName)\.git" } | 
+ForEach-Object { 
+    Write-Output "Pulling $($_.FullName)"
+    Set-Location $_.FullName
+    git pull
+    Set-Location .. 
+}; 
+
+Set-Location $current
